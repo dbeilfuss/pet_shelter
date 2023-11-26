6f7f784cfcbd14c5f8fe67386662c37e0960577e
Daniel Beilfuss <d.beilfuss@icloud.com>
2023-11-25 21:54:27 -0700

    trying name change of public folder again

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const app = express();
 app.use(cors());
 app.use(express.static("public"));
 
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/public/main.html");
-// });
-
 /// Run Server ///
 app.listen(8000, "0.0.0.0", () => {
   console.log(`Listening on 8000`);
