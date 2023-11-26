749f5fb0b2a67cb597f2c2f0d40514a377b36199
Daniel Beilfuss <d.beilfuss@icloud.com>
2023-11-25 21:48:24 -0700

    changed server back, changed name of main page to index.html

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/main.html");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/public/main.html");
+// });
 
 /// Run Server ///
 app.listen(8000, "0.0.0.0", () => {
