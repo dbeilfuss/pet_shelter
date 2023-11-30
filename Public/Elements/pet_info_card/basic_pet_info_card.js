2967aa86e253926b3e8f47a85646f6b0a5d3dcef
Daniel Beilfuss <d.beilfuss@icloud.com>
2023-11-29 22:28:34 -0700

    broke up css file into multiple css files

---
+++ Public/Elements/pet_info_card/basic_pet_info_card.js
@@ -0,0 +1,25 @@
+export function makeHeartsClickable() {
+  const favoriteButtons = document.querySelectorAll(".favorite-button");
+
+  favoriteButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      this.classList.toggle("favorited");
+      this.querySelector(".heart").classList.toggle("favorited");
+    });
+  });
+}
+
+function reserveButtonClicked() {
+  console.log("Reserve button");
+}
+
+export function makeButtonsClickable() {
+  const reserveButtons = document.querySelectorAll(".reserve-button");
+  reserveButtons.forEach((button) => {
+    button.addEventListener("click", () => reserveButtonClicked());
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => makeHeartsClickable());
+
+document.addEventListener("DOMContentLoaded", () => makeButtonsClickable());
---
+++ Public/Elements/pet_info_card/create_pet_card.js
@@ -0,0 +1,235 @@
+export function createPetCard(
+  imageURL,
+  name,
+  breed,
+  maleFemale,
+  age,
+  weight,
+  cardType
+) {
+  let petCard = `
+  <article class="pet-card">
+  <header class="pet-header">
+      <h2 class="pet-name">${name}</h2>
+      <button class="favorite-button" aria-label="Favorite ${name}">
+          <span class="heart">❤</span>
+      </button>
+  </header>
+  <img
+  src="${imageURL}"
+  alt="Photo of ${name}"
+  class="pet-photo"
+  />
+  <p class="pet-breed">${breed}</p>
+  <p class="pet-info">${age} Yrs · ${maleFemale} · ${weight} Lbs</p>
+`;
+
+  switch (cardType) {
+    case "basic":
+      petCard += `</article>`;
+      break;
+
+    case "reservable":
+      petCard += `<div class="admin-pet-buttons">
+              <input
+              type="submit"
+              name="reserve"
+              value="Reserve Pet"
+              class="reserve-button"
+              />
+          </div>
+        </article>`;
+      break;
+
+    case "Available":
+      petCard += `<div class="admin-pet-buttons">
+            <input
+            type="submit"
+            name="edit"
+            value="Edit"
+            class="edit-button"
+            />
+            <input
+            type="submit"
+            name="reserve"
+            value="Reserve"
+            class="reserve-button"
+            />
+          </div>
+        </article>`;
+      break;
+
+    case "Reserved":
+      petCard += `<div class="admin-pet-buttons">
+            <input
+            type="submit"
+            name="confirm"
+            value="Confirm Adoption"
+            class="confirm-adoption-button"
+            />
+            <input
+            type="submit"
+            name="cancel"
+            value="Cancel Reservation"
+            class="cancel-reservation-button"
+            />
+          </div>
+        </article>`;
+      break;
+
+    case "Adopted":
+      petCard += `<div class="admin-pet-buttons">
+            <input
+            type="submit"
+            name="returned"
+            value="Returned to Shelter"
+            class="returned-button"
+            />
+            <input
+            type="submit"
+            name="delete"
+            value="Delete"
+            class="delete-button"
+            />
+          </div>
+        </article>`;
+      break;
+
+    default:
+      petCard = "<p>Error Loading Pets</p>";
+  }
+
+  return petCard;
+}
+
+export function createNewPetCard() {
+  const newPetCard = `
+<form id="editPetForm" class="edit-pet-form">
+  <article class="pet-card">
+    <header class="pet-header">
+      <input
+        type="text"
+        name="name"
+        value=""
+        placeholder="Pet Name"
+        class="pet-name-input"
+      />
+    </header>
+    <img
+      src=""
+      alt=""
+      class="pet-photo"
+    />
+    <div class="pet-form-details">
+      <input
+        type="text"
+        name="imageURL"
+        value=""
+        placeholder="Image URL"
+        class="pet-image-url-input"
+      />
+      <input
+        type="text"
+        name="breed"
+        value=""
+        placeholder="Breed"
+        class="pet-breed-input"
+      />
+      <input
+        type="text"
+        name="age"
+        value=""
+        placeholder="Age in Years"
+        class="pet-age-input"
+      />
+      <input
+        type="text"
+        name="sex"
+        value=""
+        placeholder="Male / Female"
+        class="pet-sex-input"
+      />
+      <input
+        type="text"
+        name="weight"
+        value=""
+        placeholder="Weight in Lbs"
+        class="pet-weight-input"
+      />
+    </div>
+    <div class="admin-pet-buttons">
+      <input type="submit" name="save" value="Save" class="save-button" />
+      <input type="submit" name="undo" value="Undo" class="reserve-button" />
+      <input type="submit" name="delete" value="Delete" class="delete-button" />
+    </div>
+  </article>
+</form>
+`;
+  return newPetCard;
+}
+
+export function createEditablePetCard() {
+  const editablePetCard = `
+<form id="editPetForm" class="edit-pet-form">
+  <article class="pet-card">
+    <header class="pet-header">
+      <input
+        type="text"
+        name="name"
+        value="Bella"
+        placeholder="Pet Name"
+        class="pet-name-input"
+      />
+    </header>
+    <img
+      src="https://www.hartz.com/wp-content/uploads/2022/01/volunteer-at-animal-shelter-1.jpg"
+      alt=""
+      class="pet-photo"
+    />
+    <div class="pet-form-details">
+      <input
+        type="text"
+        name="imageURL"
+        value="https://www.hartz.com/wp-content/uploads/2022/01/volunteer-at-animal-shelter-1.jpg"
+        placeholder="Image URL"
+        class="pet-image-url-input"
+      />
+      <input
+        type="text"
+        name="breed"
+        value="Mix: Pit Bull Terrier"
+        placeholder="Breed: ie Mix: Pit Bull Terrier"
+        class="pet-breed-input"
+      />
+      <input
+        type="text"
+        name="age"
+        value="1"
+        placeholder="Age in Years"
+        class="pet-age-input"
+      />
+      <input
+        type="text"
+        name="sex"
+        value="Female"
+        placeholder="Male / Female"
+        class="pet-sex-input"
+      />
+      <input
+        type="text"
+        name="weight"
+        value="35"
+        placeholder="Weight in Lbs"
+        class="pet-weight-input"
+      />
+    </div>
+    <div class="admin-pet-buttons">
+      <input type="submit" name="save" value="Save" class="save-button" />
+      <input type="submit" name="undo" value="Undo" class="reserve-button" />
+      <input type="submit" name="delete" value="Delete" class="delete-button" />
+    </div>
+  </article>
+</form>
+`;
+  return editablePetCard;
+}
