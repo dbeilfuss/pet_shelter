27491561b41425a03ed3cb6f0722553675f2a88f
Daniel Beilfuss <d.beilfuss@icloud.com>
2023-12-19 21:30:15 -0700

    corrected public to Public

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-// app.use("./pet-shelter", express.static("public"));
-app.use(express.static("public"));
+app.use(express.static("Public"));
 
 /// Imports ///
 const {
